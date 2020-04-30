1015a823d77b08ee7ae91fec7021726b7a11cd13
Tenderfooted <63418253+Tenderfooted@users.noreply.github.com>
2020-05-01 04:44:14 +0800

    combat system implementation
    added the timer system, however the Actionbar doesnt sync up with it atm

--- ParacosmProject/Assets/SCripts/BattleScene.cs
+++ ParacosmProject/Assets/SCripts/BattleScene.cs
@@ -1,17 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleScene : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Slider ActionBar;
+    public GameObject PlayerActionIcon;
+    public GameObject playerIcon;                   //this is used purely to set the starting position for action bar UI elements, pretty Dodgy I know
+    public Vector3 ActionbarStart;
+
+
+
 	public static BattleScene instance;
     public GameObject Camera;
 
-	private Vector3 offset;
-    public float playertime;                // the time the player is on    when it reaches the players cast time it performs the action
-    float enemytime;                        // the current time the enemy is on when it equals their cast time they perform their action
-    bool TimerActive;                       //  is used to check if the turn is counting down*
+	private Vector3 offset; 
+
+    public float playerct;                // the time the player is on    when it reaches the players cast time it performs the action
+    public float playersct;               // the current cast time of the skill determines how far along the action bar the player must move before casting
+
+    public float playerspeed = 1;               // the players speed, determines how fast they move on the action bar
+    
+    float enemyct;                        // the current time the enemy is on when it equals their cast time they perform their action
+    public float enemysct;                // the enemies cast time for their current ability                    
+    public float enemysspeed;             // the enemy speed   
+    // bool TimerActive;                       //  is used to check if the turn is counting down*
     
  	// Use this for initialization
 	void Awake()						// set up for battle scene to become a singleton if required later on
@@ -28,12 +43,37 @@ public class BattleScene : MonoBehaviour
 	}
 	void Start () {
 	Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    ActionbarStart = playerIcon.transform.position; 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	//transform.position = Camera.transform.position ;	
+        if ( playersct > playerct)
+        {
+            //Debug.Log("timeMoving");
+            Time.timeScale = 1.0f;
+            playerct += playerspeed * Time.deltaTime;
+            enemyct += playerspeed * Time.deltaTime;
+            if(playerct > playersct)
+            {
+                Debug.Log("PlayerAbility cast!");            // **place holder** for whatever method will need to eb called to do the player ability may be 
+            }
+            if(enemyct > enemysct)
+            {
+                Debug.Log("Enemy Ability cast, OH NO");     // **place holder ** for the enemies ability animation/effects being played
+                enemyct = 0.00f;
+                enemysct =4.0f;                              // **place holder** for enemy ability putting in its cast time, 
+            }
+        }
+        else Time.timeScale = 0.0f;
+        ActionBar.value = ActionBar.value + (playerspeed* Time.deltaTime);
+        if(ActionBar.value >= 20)
+        {
+            ActionBar.value = 0.0f;
+        }
+        //else Debug.Log("timepaused");
 	    
     }
     public void EnemyDeath()
@@ -43,8 +83,23 @@ public class BattleScene : MonoBehaviour
     }
     public void InitiateCombat(OverworldEnemy Enemy)
     {
-        TimerActive = false;
+        //TimerActive = false;                              outdated timeractive boolean method for setting the time
+        enemysspeed = Enemy.speed;
+        playersct = 0.0f;
+        playerct = 0.01f;
         
+    }
+    public void TestAbilityPlaceholder()
+    {
+        Debug.Log("PewPew Chosen");
+        playerct = 0.0f;
+        playersct = 5.0f;
+        float iconpos = ActionBar.value + Time.deltaTime * 5.0f;
+        if(iconpos > 20)
+        {
+            iconpos = iconpos-20;
+        }
+        PlayerActionIcon.transform.position = new Vector3(ActionbarStart.x + iconpos, ActionbarStart.y, ActionbarStart.z);
     }
 
     // combat PsuedoCode:
@@ -78,6 +133,32 @@ public class BattleScene : MonoBehaviour
     {
         for
     }
-    I was gonna try to do a co-routine, but Ive never written one before
+     ^ I was gonna try to do a co-routine, but Ive never written one before and I realised it may not really be necessary, Unless we ??(some idea I thought of in the shower and have clearly forgotten)
+    it may be possible to make the entire battle system a co routine, so instead of being this script that plays when ever the battle object is set to active 
+    when the player bumps a enemy,
+    it is instead this piece of code that is summoned to run alongside the update loot everytime the player touches an enemy and banished when the enemies die,
+    there may be pros and cons to this system that I will think of later cause Im lazy :p    
+
+
+    to do the action bar we could literally just have a square that moves up it at a rate based on the players speed and have the time variable thing set to 0 
+    so it doesnt move, we could even have it set to a low value so the player must choose between rushing or planning their moves at the cost of time. 
+    ^this is probably the best method as we could have cool backgorund effects that play in slow motion as the player picks their move. and it should be 
+    fairly easy
+
+
+    UI:
+    player actions will probably be a list of buttons that way we can set all of their tint values at once and access them easily
+    the buttons will simply call methods from the battleController (BattleScene script)
+
+    The icons resembling enemy and player timers will lerp between two points on the action bar,  A = start of bar, B= end of bar T= equals position on bar
+    every update T = T+(playerspeed * time.deltatime)
+
+    Icons for player ability will be lerped as well at T
+    ^this wont work as Lerp Works on a range of 0-1
+    I want to simply move a box to the right but I dont know how to do that in UI ;-;
+    Screw that jazz Im just gonna use a slider with a value of 0-40 and custom images
+
+
+
     */
 }
